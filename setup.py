b81baabb1020ca9471fc583e5f07dec9981cd3b4
Niklas Karmann <niklas.s.karmann@fau.de>
2023-11-10 10:54:12 +0100

    Changes to requirements.txt and setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from setuptools import find_packages
-
-setup(
-    name="math_quiz",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=["unittest", "random"]
-)
