0193d95a4e5265a1450dce1721408d680277518b
Niklas Karmann <niklas.s.karmann@fau.de>
2023-11-10 11:22:38 +0100

    Added entry point to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup
 
 setup(
     name="math_quiz",
-    version="0.1"
+    version="0.1",
+    entry_points={
+        "console_scripts": [
+            "math_quiz = math_quiz.math_quiz:main"
+        ]
+    }
 )
